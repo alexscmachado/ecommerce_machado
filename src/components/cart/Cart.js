e32182b851f83a2b8164e5323187c4324d9fe6a7
Cíntia <111471174+crisCintia@users.noreply.github.com>
2023-05-24 21:49:18 -0300

    Update Cart.js

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -2,37 +2,45 @@ import React, { useState } from "react";
 import { useCartContext } from "../../contexts/CartContext";
 import CountItens from "../listItens/CountItens";
 import { productsDetails } from "../datails/mockDetails/productsDetails";
+import { useParams } from "react-router-dom";
 
 
 function CartItem({ }) {
     const { itens, setItens, getItemQtd, clear, removeCart,sumTotal } = useCartContext();
     const [ammount, setAmmount] = useState(1);
+    const { itemId } = useParams();
 
     function ClickOnChangeQtd(qtd){
         setAmmount(qtd);
     }
+    
+    function getProductsDetails() {
+        return new Promise((resolve, rejected) => {
+            resolve(productsDetails.find(p => p.id === parseInt(itemId)));
+        })
+    }
 
-    const cart = productsDetails.map(p => ({
+    /*const cart = productsDetails.map(p => ({
         id:p.id,
         nome: p.nome,
         imagem: p.imagem,
         preco: p.preco,
         qtd: p.qtd,
         subTotal: p.qtd * p.preco
-    }))
+    }))*/
 
     return (
         <div className="container  cardDetailsContainer">
             <div className="cardDetailImageContent" >
-                <img src={cart.imagem} className=" cardDetailsImage" alt="Laptop" />
+                <img src={itens.imagem} className=" cardDetailsImage" alt="Laptop" />
             </div>
             <div className="cardDetailsItensContent">
-                <p>{cart.nome}</p>
-                <p className=" CardDetailsNome ">R$ {cart.preco}</p>
+                <p>{itens.nome}</p>
+                <p className=" CardDetailsNome ">R$ {itens.preco}</p>
                 <p>Quantidade:  {getItemQtd()}</p>
                 {/*<CountItens stock={cardItem.estoque} qtd={ammount} onChangeQtd={ClickOnChangeQtd}><p>{getItemQtd()}</p></CountItens>*/}
                 <p>Valor Total: R$ {sumTotal}</p>
-                <button className="buyButton btn btn-primary" onClick={() => removeCart()}>Remover Item</button>
+                <button className="buyButton btn btn-primary" onClick={() => removeCart(itens.id)}>Remover Item</button>
             </div>
         </div>
     )
