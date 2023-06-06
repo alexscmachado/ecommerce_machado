3807a0437de5a67e41befac80b6a75ff2bca72a5
Cíntia <111471174+crisCintia@users.noreply.github.com>
2023-06-06 20:14:00 -0300

    Update ItemDetailsContainer.js

--- src/components/datails/ItemDetailsContainer.js
+++ src/components/datails/ItemDetailsContainer.js
@@ -1,59 +1,29 @@
 import React,{useState,useEffect} from "react";
-import { productsDetails } from "./mockDetails/productsDetails";
 import ItemDetails from "./ItemDetails";
 import { useParams } from "react-router-dom";
-import {DocumentSnapshot, doc, getDoc, collection, getDocs, getFirestore} from 'firebase/firestore';
-//import {collection, getDocs, getFirestore} from 'firebase/firestore';
+import {doc, getDoc, getFirestore} from 'firebase/firestore';
 
 
 function ItemDetailsContainer(){
-     const [myDetails, setMyDetails]=useState([])
-     const [loading, setLoading]=useState(true)
-     const {itemId}=useParams()
+    const [myDetails, setMyDetails]=useState([])
+    const [loading, setLoading]=useState(true)
+    const {itemId}=useParams()
 
-     //Desafio ItemCollection - Firebase
-
-     /*const [cardProd, setcardProd] = useState({});
-     
-     useEffect( () => {
-       const db = getFirestore()
-   
-       const refDoc = doc(db,"itemDetail", "FADaOPMK6QHX3v7ffLeR")
-       getDoc(refDoc).then((DocSnapshot) => {
-        if(DocSnapshot.exists()){
-            console.log(DocSnapshot.data())
-            setcardProd({id: DocSnapshot.id, ...DocSnapshot.data()})
-        }
-        })}, []
-    );*/
-
-    const [ProdDetail, setProdDetail] = useState([]);
-
-    useEffect(() => {
+    const getDataFromFirebase = async () => {
         const db = getFirestore();
+        const CardCollection = doc(db, "item", `${itemId}`);
+        const docSnap = await getDoc(CardCollection)
 
-        const CardCollection = collection(db, "item");
-        getDocs(CardCollection).then((snapshot) => {
-            if (snapshot.size > 0) {
-                const prodDet = snapshot.docs.map(prods => ({id: prods.id, ...prods.data()}))
-                setProdDetail(prodDet);
-            }
-        })
-    })
-    
-     function getProductsDetails(){
-       return new Promise( (resolve, rejected)=>{
-            resolve(productsDetails.find(p=>p.id ===parseInt(itemId)));
-        })
+        if (docSnap.exists()) {
+            setMyDetails({ ...docSnap.data(), id: itemId });
+            setLoading(false)
+        }
     }
 
-    useEffect(()=>{
-        setTimeout(()=>{
-            getProductsDetails()
-              .then(result => setMyDetails(result))
-        setLoading(false)
-        }, 2000)
-    })
+    useEffect(() => {
+        getDataFromFirebase()
+      }, [itemId])
+    
     if(loading){
         return(
             <div className="spinner-grow text-secondary spinnerListItens" role="status">
@@ -63,12 +33,7 @@ function ItemDetailsContainer(){
     }
     return(
         <div>
-            {/*<ItemDetails cardItem= {myDetails}/> {/*Essa é a tag original que criamos anteriormente*/}
-
-            {/*<ItemDetails cardItem={cardProd}/> {/*O produto Batom está aparecendo no fim da lista*/}
-
-            <ItemDetails cardItem={ProdDetail}/> {/*Essa é a aplicação com Firebase*/}
-
+            <ItemDetails cardItem={myDetails}/> {/*Essa é a aplicação com Firebase*/}
         </div>
     )
 }
