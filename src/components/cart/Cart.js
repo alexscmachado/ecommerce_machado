102fd2d960dabb8d8b38e93f024eb1eae92f99c4
Cíntia <111471174+crisCintia@users.noreply.github.com>
2023-06-06 20:29:40 -0300

    Update Cart.js

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,42 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import { useCartContext } from "../../contexts/CartContext";
-import { productsDetails } from "../datails/mockDetails/productsDetails";
-import CountItens from "../listItens/CountItens";
-import { useParams } from "react-router-dom";
 
 
 function CartItem({ }) {
-    const { itens, setItens, getItemQtd, clear, removeCart, sumTotal } = useCartContext();
-    const [ammount, setAmmount] = useState(1);
-    const { itemId } = useParams();
-
-    function ClickOnChangeQtd(qtd) {
-        setAmmount(qtd);
-    }
-    
-    function getProductsDetails() {
-        return new Promise((resolve, rejected) => {
-            resolve(productsDetails.find(p => p.id === parseInt(itemId)));
-        })
-    }
-
-    function getProductsDetails() {
-        return new Promise((resolve, rejected) => {
-            resolve(productsDetails.find(p => p.id === parseInt(itemId)));
-        })
-    }
+    const {  getItemQtd, removeCart, sumTotal } = useCartContext();
 
     return (
-        <div className="container  cardDetailsContainer">
+        <div className="container  cartContainer">
             <div className="cardDetailImageContent" >
-                <img src={itens.imagem} className=" cardDetailsImage" alt="Laptop" />
+                <img src={cardItem?.imagem} className=" cart-image" alt="Laptop" />
             </div>
-            <div className="cardDetailsItensContent">
-                <p>{itens.nome}</p>
-                <p className=" CardDetailsNome ">R$ {itens.preco}</p>
-                <p>Quantidade:  {getItemQtd()}</p>
-                <p>Valor Total: R$ {sumTotal}</p>
-                <button className="buyButton btn btn-primary" onClick={() => removeCart(itens.id)}>Remover Item</button>
+            <div className="carDetailsItensContent">
+                <p className="cart-title">{cardItem?.nome}</p>
+                <p className="cart-price">Preço: R$ {cardItem?.preco}</p>
+                <p className="cart-qtd">Quantidade:  {getItemQtd()}</p>
+                <p className="cart-ItemTotal">Valor Total: R$ {sumTotal}</p>
+                <button className="buyButton btn btn-secondary" onClick={() => removeCart(cardItem?.id)}>Remover Item</button>
             </div>
         </div>
     )
