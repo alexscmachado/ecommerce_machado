d9b3966e9f6bc2217fdc1cd9f060e37230437b7b
Cíntia <111471174+crisCintia@users.noreply.github.com>
2023-06-06 21:08:35 -0300

    Update CartPage.js

--- src/components/cart/CartPage.js
+++ src/components/cart/CartPage.js
@@ -1,39 +1,51 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { useCartContext } from "../../contexts/CartContext";
 import CartItem from "../cart/Cart";
-import { useParams } from "react-router-dom";
-import { collection, addDoc, getFirestore} from 'firebase/firestore';
-import { productsDetails } from "../datails/mockDetails/productsDetails";
+import { collection, addDoc, getFirestore } from 'firebase/firestore';
+import { Row, Col } from "react-bootstrap";
 
 
 function Carrinho(cardItem) {
     const { itens, clear, getTotal, createOrder } = useCartContext();
-    //const [loading, setLoading] = useState(true);
-    //const { itemId } = useParams();
+    const frete = 10.5;
+    const desconto = 0.10;
+    const [varFrete, setVarFrete] = useState(0)
+    
+    //tentando criar uma função que zere o valor do frete quando o carrinho estiver vário ou quando clicar 
+    //no botão "esvaziar carrinho"
+    function handleOnChangeFrete(FreteVar) {
+        setVarFrete(FreteVar);
+    }
 
     function salvarOrder() {
         const order = createOrder();
-
         const db = getFirestore();
         const refCollection = collection(db, "orders");
-        /*fiz certinho a função para adicionar a ordem no firebase, só que está dando erro =/, tirei print do erro e 
-        to colocando no pull request */
         addDoc(refCollection, order).then(({id}) => console.log(`O número da sua ordem de é: ${id}`))
     }
 
     return (
-        <div>
-            <ul>
-                {itens.map(p => <li key={p.id}><CartItem cardItem={p} /></li>)}
-                {/*<li><CartItem cardItem={prodCart} /></li>*/}
-            </ul>
-            <p>Valor Total da compra: R$ {getTotal()}</p>
-            <button className="buyButton btn btn-primary" onClick={createOrder}>Criar Ordem</button>
-            <p></p>
-            <button className="buyButton btn btn-primary" onClick={salvarOrder}>Salvar Ordem</button>
-            <p></p>
-            <Link><button className="buyButton btn btn-primary" onClick={() => clear()}>Esvaziar o Carrinho</button></Link>
+        <div className="cart-container">
+            <Row>
+                <Col sm={8}>
+                    <ul>
+                        {itens.map((p) => (
+                            <CartItem key={p.id} cardItem={p} />
+                        ))}
+                    </ul>
+                </Col>
+                <Col sm={4}>
+                    <p className="cartSubTotal">Subtotal: R$ {getTotal()}</p>
+                    <p className="cartSubTotal">Descontos: R${(getTotal() * desconto).toFixed(2)}</p>
+                    <p className="cartSubTotal">Frete: R$ {frete}</p>
+                    <p className="cartTotal">Total: R$ {(getTotal() - (getTotal() * desconto) + frete).toFixed(2)}</p>
+                    <button className="Button btn btn-secondary" onClick={createOrder}>Criar Ordem</button>
+                    <p></p>
+                    <button className="Button btn btn-secondary" onClick={salvarOrder}>Salvar Ordem</button>
+                    <p></p>
+                    <button className="Button btn btn-secondary" onClick={() => { clear(); handleOnChangeFrete(0); setVarFrete(0) }}>Esvaziar o Carrinho</button>
+                </Col>
+            </Row>
         </div>
     )
 }
